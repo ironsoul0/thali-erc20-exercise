4ce40f3ef8a196d354fe317db7fada0858d7dde7
Temirzhan Yussupov <timka2609@gmail.com>
2021-09-29 16:32:04 +0600

    feat: actualize Ropsten contract addresses

--- packages/frontend/config/contracts.ts
+++ packages/frontend/config/contracts.ts
@@ -6,12 +6,12 @@ import USDCArtifact from "../artifacts/contracts/USDC.sol/USDC.json";
 
 const usdcAddresses: ChainIDUrl = {
   [ChainId.Hardhat]: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
-  [ChainId.Ropsten]: "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa",
+  [ChainId.Ropsten]: "0xD8Ed07a7ad69f8E9D06A3b59847c0Fc9525F11E8",
 };
 
 const stakerAddresses: ChainIDUrl = {
   [ChainId.Hardhat]: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
-  [ChainId.Ropsten]: "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa",
+  [ChainId.Ropsten]: "0x0CECa23490E9a9E60dC52d3A0D98FCA007C86113",
 };
 
 export const USDC = {
